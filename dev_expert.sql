ec1284d8239f53aa42832a8a1cdaf76597c05b8a
JulienDaCosta-GitHub <juliendacosta27@gmail.com>
2019-09-21 23:56:51 +0200

    Correction fautes d'orthographe
    Comercial -> Commercial

--- dev_expert.sql
+++ dev_expert.sql
@@ -69,12 +69,12 @@ INSERT INTO `collaborateur` (`id`, `nom`, `prenom`, `departement_technique`, `ty
 (1, 'Paul', 'Samuel', 'Web', 'Developpeur Front', 'JeanPaulDu77', '4f45069622d55776070668bd806ab183', 'DUT MMI', '3000', 'jeanpaulprogamer@gmail.com', '0602679438'),
 (2, 'Jackson', 'Michael', 'Web', 'Designer', 'KingOfPop', '8349383df2c74838e67ce0938b3145e7', 'DUT MMI', '7000', 'mjking@gmail.com', '0785497883'),
 (3, 'Zidane', 'Zinedine', 'Sécurité', 'Ingénieur', 'Zizou2002', '27de2871a8a95fe55e96b895f6842b35', 'DUT Informatique', '5000', 'zizoufoot@gmail.com', '0798546389'),
-(4, 'Stark', 'Tony', 'Comercial', 'Comptable', 'IronMan', '9f3951251a74d7c89605fc26d1a2f38b', 'DUT Economie', '10000', 'avengersnumerone@gmail.com', '0215779632'),
+(4, 'Stark', 'Tony', 'Commercial', 'Comptable', 'IronMan', '9f3951251a74d7c89605fc26d1a2f38b', 'DUT Economie', '10000', 'avengersnumerone@gmail.com', '0215779632'),
 (5, 'Rogers', 'Steve', 'Sécurité', 'Ingénieur', 'CaptainAmerica', 'faae70aa6949a759b90a0b2d5982e587', 'DUT Economie', '4000', 'icandothisallday@gmail.com', '0754698854'),
 (6, 'Parker', 'Peter', 'Web', 'Designer', 'TheAmazingSpiderman2000', '555348ae3a35204b22db9439dc30405a', 'Bachelor Design', '5000', 'peterparker@gmail.com', '0365878996'),
 (7, 'Potter', 'Harry', 'Réalité Virtuelle', 'Testeur', 'CeluiQuiASurvecu', '0f66e9f482711e5d8c0693eea4a4918d', 'DUT Informatique', '8000', 'magicienextreme@gmail.com', '0456996882'),
-(8, 'Kenobi', 'Obi Wan', 'Comercial', 'Comptable', 'TheHighGround_82', '873da653647d18645c812b4375ddc80c', 'DUT Economie', '6500', 'hellothere@gmail.com', '0458799825'),
-(9, 'Skywalker', 'Anakin', 'Comercial', 'Comptable', 'DarthVader_93', '8e0a4747db147107b5ced2eced359f41', 'DUT Economie', '6500', 'jenaimepaslesable@gmail.com', '0265588525'),
+(8, 'Kenobi', 'Obi Wan', 'Commercial', 'Comptable', 'TheHighGround_82', '873da653647d18645c812b4375ddc80c', 'DUT Economie', '6500', 'hellothere@gmail.com', '0458799825'),
+(9, 'Skywalker', 'Anakin', 'Commercial', 'Comptable', 'DarthVader_93', '8e0a4747db147107b5ced2eced359f41', 'DUT Economie', '6500', 'jenaimepaslesable@gmail.com', '0265588525'),
 (10, 'Windu', 'Mace', 'Web', 'Developpeur Back', 'SamuelLeeJackson-Pro', 'a3e8872a8e629ae4f37d54a8e9467fd9', 'DUT MMI', '9000', 'purplelightsaber@gmail.com', '0654789856');
 
 -- --------------------------------------------------------
